f60c0f1b6874c52a597eaa4c70cecbd956f7871e
luthfifr <luthfir272@gmail.com>
2020-04-10 18:07:13 +0700

    change the main file to answer the challenge

--- README.md
+++ README.md
@@ -2,9 +2,8 @@
 
 Course Name: The Complete 2020 Flutter Development Bootcamp with Dart
 
-Section: 3
+Section: 6
 
 ## Screenshots
-- App Icon: ![](iar-icon.png)
-- Homescreen: ![](iar-homescreen.png)
+- Homescreen: ![](layout-challenge-answer.png)
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,49 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          title: Text('I am Rich'),
-          backgroundColor: Colors.blueGrey[900],
-        ), 
-        body: Center(
-          child: Image(
-            image: AssetImage('images/diamond.png'),
-          ),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                color: Colors.red,
+                width: 100.0,
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: Colors.yellow[300],
+                    ),
+                  ],
+                )
+              ),
+              Container(
+                color: Colors.blue,
+                width: 100.0,
+              )
+            ]
+          )
         ),
       ),
-    ),
-  );
+    );
+  }
 }
