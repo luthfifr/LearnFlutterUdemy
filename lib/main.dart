7849931a361a6be0ef892caa9d9251d873f993f2
luthfifr <luthfir272@gmail.com>
2020-04-16 10:06:25 +0700

    still trying to create func

--- lib/main.dart
+++ lib/main.dart
@@ -31,19 +31,24 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: diceBtnPressed(),
               child: Image.asset('images/dice1.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice2.png'),
-            ),
+            child: FlatButton(
+              onPressed: () {
+                print('right button');
+              },
+              child: Image.asset('images/dice2.png')),
           ),
         ],
       ),
     );
+  }
+
+  void diceBtnPressed() {
+    print('left button');
   }
 }
